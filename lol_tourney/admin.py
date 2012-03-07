8aa3947f3330a04ecf322b0ae13fc886f4a80855
James P Rowe <jrowe7@mace.cs.byu.edu>
2012-03-06 20:11:57 -0700

    Yarr. Moved it over to the Python branch for future development on this

--- lol_tourney/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from lol_tourney.models import Summoner
-
-class SummonerAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Summoner, SummonerAdmin)
--- lol_tourney/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-
-urlpatterns = patterns('lol_tourney.views',
-    # Examples:
-    #url(r'^about/$', 'about', name='About'),
-    url(r'^matches/$', 'matches', name='Current Matches'),
-)
